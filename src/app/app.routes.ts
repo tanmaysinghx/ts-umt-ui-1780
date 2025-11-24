699f0c625853e264f8a642695437c1ff43f62dae
Tanmay Singh <tanmaysinghx@gmail.com>
2025-11-24 23:02:27 +0530

    Clean Up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,6 @@ import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.co
 import { ServerErrorPageComponent } from './shared/server-error-page/server-error-page.component';
 import { UserChatsComponent } from './features/communication/user-chats/user-chats.component';
 import { MyProfileComponent } from './features/my-section/my-profile/my-profile.component';
-import { ProfileDetailsComponent } from './features/account-id-creation/profile-details/profile-details.component';
-import { CompanyDetailsComponent } from './features/account-id-creation/company-details/company-details.component';
-import { SubscriptionDetailsComponent } from './features/account-id-creation/subscription-details/subscription-details.component';
-import { TermsConditionsReviewComponent } from './features/account-id-creation/terms-conditions-review/terms-conditions-review.component';
 import { UserListComponent } from './features/user-management/user-list/user-list.component';
 import { DashboardApplicationsComponent } from './features/dashboard/dashboard-applications/dashboard-applications.component';
 import { MyAccountComponent } from './features/my-section/my-account/my-account.component';
@@ -38,12 +34,6 @@ export const routes: Routes = [
     //dashboard urls
     { path: 'dashboard/applications', component: DashboardApplicationsComponent },
     { path: 'dashboard/application-favorites', component: DashboardApplicationsComponent },
-
-    //account-id-creation urls
-    { path: 'account-id-creation/profile-details', component: ProfileDetailsComponent },
-    { path: 'account-id-creation/company-details', component: CompanyDetailsComponent },
-    { path: 'account-id-creation/subscription-details', component: SubscriptionDetailsComponent },
-    { path: 'account-id-creation/terms-conditions-review', component: TermsConditionsReviewComponent },
 
     //service urls
     { path: 'maintenance-page', component: MaintenancePageComponent },
